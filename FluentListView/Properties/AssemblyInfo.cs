983fd82aa547df1c67c0f52760085f88670f31a0
Robin Rodricks <robinrodricks7@gmail.com>
2019-03-06 12:41:41 +0530

    rebrand to FluentListView

---
+++ FluentListView/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FluentListView")]
+[assembly: AssemblyDescription("FluentListView is a C# wrapper around a .NET ListView, supporting model-bound lists, in-place item editing, drag and drop, icons, themes, trees & data grids, and much more.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Robin Rodricks & Contributors")]
+[assembly: AssemblyProduct("FluentListView")]
+[assembly: AssemblyCopyright("Copyright © Robin Rodricks & Contributors 2006-2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ef28c7a8-77ae-442d-abc3-bb023fa31e57")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.0.0")]
+[assembly: AssemblyInformationalVersion("2.9.1")]
+[assembly: System.CLSCompliant(true)]
